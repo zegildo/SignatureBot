b40b76031d187342d66104895a4dd5f5ec52a540
José <zegildo@gmail.com>
2020-04-09 17:53:46 -0300

    Cade Signature Builder
    Modificação do projeto para permitir a criação de uma assinatura de e-mail personalizada para os membros do CADE.

---
+++ src/conf/settings.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+PASSWORD = os.getenv("PASSWORD")
+MSG_START = os.getenv("MSG_START")
+MSG_ORGAO = os.getenv("MSG_ORGAO")
+MSG_SENHA = os.getenv("MSG_SENHA")
+MSG_SENHA_NOK = os.getenv("MSG_SENHA_NOK")
+MSG_TITULO = os.getenv("MSG_TITULO")
+MSG_CARGO = os.getenv("MSG_CARGO")
+MSG_DDD = os.getenv("MSG_DDD")
+MSG_TEL = os.getenv("MSG_TEL")
+MSG_LOG = os.getenv("MSG_LOG")
---
+++ src/signature_builder.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to generate automatic e-mail signatures.
+
+from PIL import Image, ImageFont, ImageDraw, ImageEnhance
+from unicodedata import normalize
+from io import BytesIO
+
+def get_nome_titulo(nome, titulo):
+	nome_e_titulo = nome +", "+ titulo
+	return nome_e_titulo
+
+def get_telefone(DDD, tel):
+	telefone = tel.replace(' ','-')
+	telefone_completo = '+55 '+DDD+' '+telefone
+	return telefone_completo
+
+def get_email(login):
+	email = login+'@cade.gov.br'
+	return email
+
+def get_cargo(cargo):
+	cargo = cargo + " - DEE"
+	return cargo
+
+def create_personal_information(kwargs):
+	'''
+	'''
+	nome_e_titulo = get_nome_titulo(kwargs['NOME'], kwargs['TITULO'])
+	cargo = get_cargo(kwargs['CARGO'])
+	telefone = get_telefone(kwargs['DDD'], kwargs['TELEFONE'])
+	email = get_email(kwargs['LOGIN'])
+
+	img = Image.new('RGB', (200, 100), color = (255, 255, 255))
+	fnt_nome = ImageFont.truetype('fonts/Effra_Std_Rg.ttf', 17)
+	fnt_inf = ImageFont.truetype('fonts/roboto/Roboto-Regular.ttf', 12)
+
+	d = ImageDraw.Draw(img)
+	
+	d.text((10,10), nome_e_titulo, font=fnt_nome, fill=(0, 102, 51))
+	d.text((10,33), cargo, font=fnt_inf, fill=(36, 62, 106))
+	d.text((10,53), telefone, font=fnt_inf, fill=(36, 62, 106))
+	d.text((10,73), email, font=fnt_inf, fill=(36, 62, 106))
+	return img
+
+def create_logo():
+	'''
+	'''
+	logo = Image.open("logos/cade/cade.jpg")
+	return logo
+
+def get_file_name(nome):
+	'''
+	'''
+	name_lower_case = nome.lower()
+	name_without_accents = normalize('NFKD', name_lower_case).encode('ASCII', 'ignore').decode('ASCII')
+	name_surname = name_without_accents.split()[:2]
+	file_name = '.'.join(name_surname)
+	extension = '.png'
+	file_name_final = file_name + extension
+	return file_name_final
+
+
+def create(kwargs):
+	'''
+	'''
+	logo = 	create_logo()
+	img_personal_data = create_personal_information(kwargs)
+	nome_file = get_file_name(kwargs['NOME'])
+
+	new_im = Image.new('RGB', (350,110), "white")
+	new_im.paste(logo,(10, 10))
+	new_im.paste(img_personal_data,(160, 0))
+
+	memory = BytesIO()
+	memory.name = nome_file
+	new_im.save(memory, quality = 100)
+	memory.seek(0)
+
+	return memory
