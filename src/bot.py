ab18b097914163e74e73fe5abacbf953fbd693e4
José <zegildo@gmail.com>
2020-04-17 10:35:14 -0300

    Creation Continuous Integration
    DEV and PROD mode

--- src/bot.py
+++ src/bot.py
@@ -8,15 +8,17 @@ from telegram import (ParseMode, ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           ConversationHandler)
 
-from conf.settings import (TELEGRAM_TOKEN, PASSWORD, MSG_START, MSG_ORGAO,
-                        MSG_SENHA,  MSG_SENHA_NOK, MSG_TITULO, MSG_CARGO, MSG_DDD, 
-                        MSG_TEL, MSG_LOG)
+from conf.settings import (MODE, HEROKU_APP_NAME, PORT, TELEGRAM_TOKEN, PASSWORD)
+
+from conf.messages_pt import (MSG_START, MSG_ORGAO, MSG_ORGAO_NOK, MSG_SENHA,
+                              MSG_SENHA_NOK, MSG_TITULO, MSG_CARGO, MSG_DDD,
+                              MSG_TEL, MSG_LOG)
+
 import signature_builder as sb
 
-
-
-logging.basicConfig(filename='../log/log.log',level=logging.INFO)
-logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
 
 ORGAO, SENHA, TITULO, CARGO, DDD, TELEFONE, LOGIN, IMAGEM = range(8) 
 EMAIL_SIGNATURE_PARAMS = {}
@@ -25,9 +27,9 @@ def start(update, context):
     """
     """
     reply_keyboard = [['CADE', 'UFERSA']]
-
     update.message.reply_text(MSG_START,
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard,
+        resize_keyboard=True))
 
     return ORGAO
 
@@ -35,11 +37,22 @@ def orgao(update, context):
     """
     """
     user = update.message.from_user
+    orgao = update.message.text
     logger.info("User %s: %s", user.first_name, update.message.text)
-    EMAIL_SIGNATURE_PARAMS['ORGAO'] = update.message.text
-    update.message.reply_text(MSG_ORGAO, parse_mode=ParseMode.MARKDOWN_V2)
-    return SENHA
 
+    orgao = orgao.upper()
+    if orgao in ['UFERSA', 'CADE']:
+        EMAIL_SIGNATURE_PARAMS['ORGAO'] = update.message.text
+        update.message.reply_text(MSG_ORGAO, 
+            reply_markup=ReplyKeyboardRemove(),
+            parse_mode=ParseMode.MARKDOWN_V2)
+        return SENHA
+    else:
+        update.message.reply_text(MSG_ORGAO_NOK, 
+            reply_markup=ReplyKeyboardRemove())
+        return ConversationHandler.END
+
+    
 def senha(update, context):
     """
     """
@@ -50,7 +63,8 @@ def senha(update, context):
         update.message.reply_text(MSG_SENHA_NOK)
         return ConversationHandler.END
     else:
-        update.message.reply_text(MSG_SENHA, parse_mode=ParseMode.MARKDOWN_V2)
+        update.message.reply_text(MSG_SENHA, 
+            parse_mode=ParseMode.MARKDOWN_V2)
     
     logger.info("User %s: senha: %s", user.first_name, update.message.text)
 
@@ -63,12 +77,10 @@ def titulo(update, context):
     EMAIL_SIGNATURE_PARAMS['NOME'] = update.message.text
     logger.info("User %s: %s", user.first_name, update.message.text)
     
-
     reply_keyboard = [['BSc.', 'Esp.', 'MBA.', 'MsC.','Ph.D.']]
     update.message.reply_text(MSG_TITULO,
             reply_markup=ReplyKeyboardMarkup(reply_keyboard, 
-            resize_keyboard=True,
-            one_time_keyboard=True))
+            resize_keyboard=True))
 
     return CARGO
 
@@ -79,8 +91,9 @@ def cargo(update, context):
     logger.info("User %s: %s", user.first_name, update.message.text)
     EMAIL_SIGNATURE_PARAMS['TITULO'] = update.message.text
 
-    reply_keyboard = [['BSc.', 'Esp.', 'MBA.', 'MsC.','Ph.D.']]
-    update.message.reply_text(MSG_CARGO)
+    update.message.reply_text(MSG_CARGO, 
+        reply_markup=ReplyKeyboardRemove())
+
     return DDD
 
 def DDD(update, context):
@@ -89,7 +102,9 @@ def DDD(update, context):
     user = update.message.from_user
     logger.info("User %s: %s", user.first_name, update.message.text)
     EMAIL_SIGNATURE_PARAMS['CARGO'] = update.message.text
-    update.message.reply_text(MSG_DDD)
+
+    update.message.reply_text(MSG_DDD, parse_mode=ParseMode.MARKDOWN_V2)
+
     return TELEFONE
 
 def telefone(update, context):
@@ -97,8 +112,10 @@ def telefone(update, context):
     """
     user = update.message.from_user
     EMAIL_SIGNATURE_PARAMS['DDD'] = update.message.text
+
     logger.info("User %s: %s", user.first_name, update.message.text)
-    update.message.reply_text(MSG_TEL)
+    update.message.reply_text(MSG_TEL, parse_mode=ParseMode.MARKDOWN_V2)
+    
     return LOGIN
 
 def login(update, context):
@@ -108,7 +125,9 @@ def login(update, context):
     EMAIL_SIGNATURE_PARAMS['TELEFONE'] = update.message.text
     logger.info("User %s: %s", user.first_name, update.message.text)
     update.message.reply_text(MSG_LOG,
+        reply_markup=ReplyKeyboardRemove(),
         parse_mode=ParseMode.MARKDOWN_V2)
+
     return IMAGEM
 
 def imagem(update, context):
@@ -118,7 +137,6 @@ def imagem(update, context):
     EMAIL_SIGNATURE_PARAMS['LOGIN'] = update.message.text
     logger.info("User %s: %s", user.first_name, update.message.text)
 
-    
     img = sb.create(EMAIL_SIGNATURE_PARAMS)
     context.bot.send_photo(user.id, photo=img)
 
@@ -150,34 +168,37 @@ def main():
         entry_points = [CommandHandler('start', start)],
 
         states = {
-            ORGAO: [MessageHandler(Filters.regex('^(CADE|UFERSA)$'), orgao)],
-
+            ORGAO: [MessageHandler(Filters.text, orgao)],
             SENHA: [MessageHandler(Filters.text, senha)],
-
             TITULO: [MessageHandler(Filters.text, titulo)],
-            
             CARGO: [MessageHandler(Filters.text, cargo)],
-
             DDD: [MessageHandler(Filters.text, DDD)],
-            
             TELEFONE: [MessageHandler(Filters.text, telefone)],
-
             LOGIN: [MessageHandler(Filters.text, login)],
-
             IMAGEM: [MessageHandler(Filters.text, imagem)]
         },
 
         fallbacks = [CommandHandler('cancel', cancel)]
     )
 
+    
+
     dp.add_handler(conv_handler)
     dp.add_error_handler(error)
 
-    # Start the Bot
-    updater.start_polling()
+    if(MODE == "PROD"):
+        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TELEGRAM_TOKEN)
+        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TELEGRAM_TOKEN))
+        
+    elif(MODE == "DEV"):
+        updater.start_polling()
+        logger.warning('MODE:DEV - start_polling - OK')
+
+    else:
+        logger.error("No MODE specified!")
+        sys.exit(1)
+
     updater.idle()
 
-
 if __name__ == '__main__':
-    print("press CTRL + C to cancel.")
     main()
--- src/conf/settings.py
+++ src/conf/settings.py
@@ -4,20 +4,8 @@
 
 import os
 
-#from dotenv import load_dotenv
-
-#load_dotenv()
-
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 PASSWORD = os.environ.get("PASSWORD")
 PORT = os.environ.get("PORT")
-#MSG_START = os.getenv("MSG_START")
-#MSG_ORGAO = os.getenv("MSG_ORGAO")
-#MSG_SENHA = os.getenv("MSG_SENHA")
-#MSG_SENHA_NOK = os.getenv("MSG_SENHA_NOK")
-#MSG_TITULO = os.getenv("MSG_TITULO")
-#MSG_CARGO = os.getenv("MSG_CARGO")
-#MSG_DDD = os.getenv("MSG_DDD")
-#MSG_TEL = os.getenv("MSG_TEL")
-#MSG_LOG = os.getenv("MSG_LOG")
+MODE = os.environ.get("MODE")
